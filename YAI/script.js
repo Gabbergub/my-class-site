67392df02a5010bbf9b980b358ef76ae5d8a1caf
Gabe Marroquin <144389166+Gabbergub@users.noreply.github.com>
2023-11-17 01:23:22 -0500

    Add files via upload

---
+++ YAI/script.js
@@ -0,0 +1,17 @@
+setInterval(function(){
+    let t = document.getElementById("triangle");
+let randomSize1 = Math.random() * (500 - 50 + 1) + 50;
+let randomSize2 = Math.random() * (500 - 50 + 1) + 50;
+let randomSize3 = Math.random() * (500 - 50 + 1) + 50;
+
+
+// TRIANGLE SIZE
+let randomHeight1 = Math.random() * (500 - 50 + 1) + 50;
+let randomHeight2 = Math.random() * (400 - 50 + 1) + 50;
+let randomHeight3 = Math.random() * (400 - 50 + 1) + 50;
+
+t.style.borderTop = randomHeight1 + "px solid transparent";
+    t.style.top = -randomHeight1 + "px solid orange";
+    t.style.top = randomHeight1 + "px solid transparent"
+    t.style.transform = "rotate(" +  randomRotate1 + "deg)";
+},1000);
